39ef31b52d96db42c4ee333c46276c2931059b11
Shreya Haldia <shaldia@confluent.io>
2023-11-03 17:38:17 +0530

    Create produce_promotion_info.py

---
+++ python-scripts/produce_promotion_info.py
@@ -0,0 +1,45 @@
+import random
+import json
+import faker
+from confluent_kafka import Producer
+
+# Create a Faker instance
+fake = faker.Faker()
+
+# Kafka configuration
+conf = {
+    'bootstrap.servers': 'pkc-921jm.us-east-2.aws.confluent.cloud:9092',        # Add your Kafka broker(s) here
+    'sasl.mechanisms': 'PLAIN',
+    'security.protocol': 'SASL_SSL',
+    'sasl.username': 'KHMDL6NACBUOJ2TA',            # Add your SASL username
+    'sasl.password': 'dBpH4Ee9cM7PSwwsrueKVCnyt8R0bNxnvsvQkdbEfIg2LGiFeARsBBU0hP93wjYb'             # Add your SASL password
+}
+
+# Function to generate mock promotion data
+def produce_promotion_info(producer, num_promotions):
+    while True:
+        promotion_info = []
+        for _ in range(num_promotions):
+            promotion = {
+                'pid': fake.uuid4(),                      # Generate a random UUID as pid
+                'SKU': fake.random_int(min=1000, max=9999),  # SKU
+                'segment': fake.random_element(elements=('SegmentA', 'SegmentB', 'SegmentC'))  # Segment
+            }
+            promotion_info.append(promotion)
+
+        # Produce the promotion information to the Kafka topic
+        for promotion in promotion_info:
+            promotion_data_json = json.dumps(promotion)
+            producer.produce("products-promotions", value=promotion_data_json)
+
+        # Wait for any outstanding messages to be delivered and delivery reports to be received
+        producer.flush()
+
+def main():
+    p = Producer(conf)
+    num_promotions = 1 # Change this number to generate more or fewer promotions
+    produce_promotion_info(p, num_promotions)
+    print(f"Produced {num_promotions} promotion events to products.promotions topic.")
+
+if __name__ == "__main__":
+    main()
